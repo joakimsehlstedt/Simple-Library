58b986dbec5efa63b9a1b93a44f0a01646d588b1
Joxiba <joxiba@icloud.com>
2014-11-12 13:52:38 +0000

    Init update

--- Library/Models/Book.cs
+++ Library/Models/Book.cs
@@ -15,7 +15,7 @@ namespace Library.Models {
         [Required]
         public Author Author { get; set; }
         public virtual ICollection<BookCopy> BookCopies { get; set; }
-
+         
         public override string ToString() {
             return String.Format("[{0}] -- {1}", this.Id, this.Title);
         }
